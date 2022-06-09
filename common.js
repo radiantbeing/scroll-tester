9d993f0fd2c1279a5b4f66cf41e195f14f66fbbb
INHWA KIM <radiantbeing99@gmail.com>
2022-06-10 00:07:14 +0900

    feat: Add setting for number of paragraphs
    * Added an input that lets users set number of paragraphs to generate.
    * Modified the design of the loading bar in detail.

--- common.js
+++ common.js
@@ -2,36 +2,51 @@
   const res = await fetch("lorem.json");
   const result = await res.json();
 
-  const quantity = 1000;
   const body = QS("#loremBody");
   const loadingBar = QS("#loadingBar");
   const loadingInfo = QS("#loadingInfo");
 
-  const data = { text: result, quantity: quantity, body: body, loadBar: loadingBar, loadInfo: loadingInfo };
+  QS("#runBtn").addEventListener("click", async (event) => {
+    body.innerHTML = "";
 
-  for (let i = 0; i < quantity; i++) {
-    await appendParagraph(data, i);
-  }
+    const paragraphsNum = QS("#paragraphsNum");
+    const quantity = parseInt(paragraphsNum.value);
+
+    if (isNaN(quantity)) {
+      alert("Please enter an integer value");
+      paragraphsNum.value = null;
+      paragraphsNum.focus();
+      return;
+    }
+
+    QS(".loading-wrapper").style.display = "block";
+
+    const data = { text: result, quantity: quantity, body: body, loadBar: loadingBar, loadInfo: loadingInfo };
+
+    for (let i = 0; i < quantity; i++) {
+      await appendParagraph(data, i);
+    }
+  });
 })();
 
 function QS(selector) {
   return document.querySelector(selector);
 }
 
-function createEl(name) {
-  return document.createElement(name);
-}
-
 async function appendParagraph(data, index) {
   setTimeout(() => {
-    data.loadBar.style.width = `${(index / data.quantity) * 100}%`;
-    data.loadInfo.textContent = `Generated paragraph: ${index + 1}/${data.quantity}`;
+    renewLoadBar(data.loadBar, data.loadInfo, index, data.quantity);
 
     const rand = Math.round(Math.random() * 8) + 1;
     const paragraphText = data.text[rand] + " " + data.text[rand] + " " + data.text[rand];
-    const newPara = createEl("p");
+    const newPara = document.createElement("p");
     newPara.innerText = "\t" + paragraphText;
 
     data.body.appendChild(newPara);
   }, 0);
+
+  function renewLoadBar(loadBar, loadInfo, index, quantity) {
+    loadBar.style.width = `${(index / quantity) * 100}%`;
+    loadInfo.textContent = `Generated paragraphs: ${index + 1}/${quantity}`;
+  }
 }
